2e4b3350367e3788229ef45785b80b082ad5b457
Towarisz <matis2k4@gmail.com>
2022-04-16 17:48:02 +0200

    home page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PricePageComponent } from './price-page/price-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    PricePageComponent,
+    FaqPageComponent,
+    AboutPageComponent,
+    NewsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomePageComponent },
+      { path: 'price', component: PricePageComponent },
+      { path: 'faq', component: FaqPageComponent },
+      { path: 'about', component: AboutPageComponent },
+    ]),
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/news/news.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss'],
+})
+export class NewsComponent {
+  @Input() list: { title: String; content: String } | undefined;
+  constructor() {}
+}

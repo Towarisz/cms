a77c279ff368d5d9ce39cfdb8c2a7ffc8f84776d
Towarisz <matis2k4@gmail.com>
2022-05-23 16:01:58 +0200

    kurwa dziala nie wiem jak nie pytaj

--- src/app/theme-page/theme-page.component.ts
+++ src/app/theme-page/theme-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-theme-page',
   templateUrl: './theme-page.component.html',
-  styleUrls: ['./theme-page.component.scss']
+  styleUrls: ['./theme-page.component.scss'],
 })
-export class ThemePageComponent implements OnInit {
-  select:string = "";
-  constructor() { }
-
-  ngOnInit(): void {
+export class ThemePageComponent {
+  rootElement: any;
+  theme: number = 1;
+  constructor() {
+    this.rootElement = document.documentElement;
   }
 
+  setTheme() {
+    switch (this.theme) {
+      case 1:
+        this.rootElement.style.setProperty('--background', 'white');
+        this.rootElement.style.setProperty('--font-color', 'black');
+        break;
+      case 2:
+        this.rootElement.style.setProperty('--background', 'rgb(44, 40, 40)');
+        this.rootElement.style.setProperty('--font-color', 'whitesmoke');
+        break;
+      case 3:
+        this.rootElement.style.setProperty('--background', 'rgb(44, 40, 40)');
+        this.rootElement.style.setProperty('--font-color', 'whitesmoke');
+        break;
+      default:
+        break;
+    }
+  }
 }

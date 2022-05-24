1ce08b3786a4da7568a046b2783a7d5ddcd36edc
Towarisz <matis2k4@gmail.com>
2022-05-24 13:51:13 +0200

    u

--- src/app/theme-page/theme-page.component.ts
+++ src/app/theme-page/theme-page.component.ts
@@ -17,14 +17,26 @@ export class ThemePageComponent {
       case 1:
         this.rootElement.style.setProperty('--background', 'white');
         this.rootElement.style.setProperty('--font-color', 'black');
+        this.rootElement.style.setProperty('--second-background', 'white');
+        this.rootElement.style.setProperty('--second-font-color', 'black');
         break;
       case 2:
-        this.rootElement.style.setProperty('--background', 'rgb(44, 40, 40)');
+        this.rootElement.style.setProperty('--background', 'rgb(21, 24, 24)');
         this.rootElement.style.setProperty('--font-color', 'whitesmoke');
+        this.rootElement.style.setProperty('--second-background', 'grey');
+        this.rootElement.style.setProperty('--second-font-color', 'black');
         break;
       case 3:
         this.rootElement.style.setProperty('--background', 'rgb(44, 40, 40)');
         this.rootElement.style.setProperty('--font-color', 'whitesmoke');
+        this.rootElement.style.setProperty('--second-background', 'grey');
+        this.rootElement.style.setProperty('--second-font-color', 'whitesmoke');
+        break;
+      case 4:
+        this.rootElement.style.setProperty('--background', 'rgb(44, 40, 40)');
+        this.rootElement.style.setProperty('--font-color', 'whitesmoke');
+        this.rootElement.style.setProperty('--second-background', 'white');
+        this.rootElement.style.setProperty('--second-font-color', 'black');
         break;
       default:
         break;
